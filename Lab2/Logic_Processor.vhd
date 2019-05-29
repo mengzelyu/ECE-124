f48f7d26cb4c4a38cee9be38c0d388f7bdbb8025
mengzelyu <51139549+mengzelyu@users.noreply.github.com>
2019-05-29 14:20:44 -0400

    Update Logic_Processor.vhd

--- Lab2/Logic_Processor.vhd
+++ Lab2/Logic_Processor.vhd
@@ -4,10 +4,10 @@ use ieee.numeric_std.all;
 
 entity Logic_Processor is port (
    
-   pb					: in	std_logic_vector(2 downto 0); 
+   pb				: in	std_logic_vector(2 downto 0); 
    hex_A	   		: in  std_logic_vector(3 downto 0); 
-   hex_B	 		   : in  std_logic_vector(7 downto 4);
-	Logic_Func		: out std_logic_vector(3 downto 0)
+   hex_B	 		: in  std_logic_vector(7 downto 4);
+   Logic_Func			: out std_logic_vector(3 downto 0)
 
 ); 
 end Logic_Processor;
@@ -16,9 +16,9 @@ architecture Behavioral of Logic_Processor is
 
 begin	
 	with pb select						      
-	Logic_Func 				    <= hex_A AND hex_B when "110",    
-										 hex_A OR hex_B when "101", 
-										 hex_A XOR hex_B when "011",
-										 "0000" when others;
+	Logic_Func <= hex_A AND hex_B when "110",    
+		      hex_A OR hex_B when "101", 
+		      hex_A XOR hex_B when "011",
+		      "0000" when others;
 	
 end architecture Behavioral; 
